36fa65935f76c8003cd72994f0fba965ecdccf46
kirsguo <kirsguo@163.com>
2017-04-10 17:01:26 +0800

    chapter one （test）

---
+++ chapter1/HelloWorld/app/src/main/java/com/example/kirsguo/helloworld/HelloWorldActivity.java
@@ -0,0 +1,17 @@
+package com.example.kirsguo.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        Log.d("HelloWorldActivity","onCreate execute");
+
+    }
+}
